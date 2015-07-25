0e0af2feafb111fcb609ea3432e1e3f09ca16412
ImKingLouie <imkinglouie@gmail.com>
2015-07-25 11:41:41 -0500

    Create search.php

---
+++ app/search.php
@@ -0,0 +1,191 @@
+<?php
+	require_once(__DIR__ . "/func.php");
+	$arr=false;
+	if(isset($_POST["submit"]))
+		switch($_POST["category"]){
+			case "rooms":
+				switch($_POST["number"]){
+					case 1:
+						$arr=a1();
+					break;
+					case 2:
+						$arr=a2();
+					break;
+					case 3:
+						$arr=a3();
+					break;
+					default:
+					die("invalid post");
+				}
+			break;
+			case "patients":
+				switch($_POST["number"]){
+					case 1:
+						$arr=b1();
+					break;
+					case 2:
+						$arr=b2();
+					break;
+					case 3:
+						$arr=b3();
+					break;
+					case 4:
+						$arr=b4();
+					break;
+					case 5:
+						$arr=b5();
+					break;
+					case 6:
+						$arr=b6();
+					break;
+					case 7:
+						$arr=b7();
+					break;
+					case 8:
+						$arr=b8();
+					break;
+					case 9:
+						$arr=b9();
+					break;
+					case 10:
+						$arr=b10();
+					break;
+					default:
+					die("invalid post");
+				}
+			break;
+			case "treatments":
+				switch($_POST["number"]){
+					case 1:
+						$arr=c1();
+					break;
+					case 2:
+						$arr=c2();
+					break;
+					case 3:
+						$arr=c3();
+					break;
+					case 4:
+						$arr=c4();
+					break;
+					case 5:
+						$arr=c5();
+					break;
+					case 6:
+						$arr=c6();
+					break;
+					case 7:
+						$arr=c7();
+					break;
+					case 8:
+						$arr=c8();
+					break;
+					default:
+					die("invalid post");
+				}
+			break;
+			case "employees":
+				switch($_POST["number"]){
+					case 1:
+						$arr=d1();
+					break;
+					case 2:
+						$arr=d2();
+					break;
+					case 3:
+						$arr=d3();
+					break;
+					case 4:
+						$arr=d4();
+					break;
+					case 5:
+						$arr=d5();
+					break;
+					case 6:
+						$arr=d6();
+					break;
+					case 7:
+						$arr=d7();
+					break;
+					default:
+					die("invalid post");
+				}
+			break;
+			default:
+			die("invalid post");
+		}
+?>
+<html>
+<head>
+	<title>Brookhaven Hospital</title>
+	<style>
+		tr:nth-child(even){
+			background-color:lightgray;
+		}
+	</style>
+</head>
+<body>
+	<form id="search" method="post" enctype="multipart/form-data" action="search.php"></form>
+	<table id="results">
+<?php
+//if there is data to print
+	if($arr){
+	//print headers
+		echo"<tr>";
+		foreach(array_keys($arr) as $row)
+			echo"<td>".$row."</td>";
+		echo"</tr>";
+	//print data under headers
+		foreach($arr as $row){
+			echo"<tr>";
+				foreach($row as $col){
+					echo"<td>".$col."</td>";
+				}
+			echo"</tr>";
+		}
+	}
+?>
+	</table>
+	<script type="text/javascript">
+		(function(parent){
+		//problem categories and numbers
+			var categories={
+				rooms:3,
+				patients:10,
+				treatments:8,
+				employees:7
+			},
+		//select element for problem category
+			category=document.createElement("select"),
+		//select element for problem number
+			number=document.createElement("select"),
+		//submit button
+			submit=document.createElement("input"),
+		//repopulate number select element if category select element is changed
+			switchCategory=function f(){
+				while(number.firstChild)
+					number.removeChild(number.firstChild);
+				for(var i=1;i<=categories[category.value];i++)
+					addOption(number,i,i);
+				return f; //<--I'm an artist.
+			}();
+			submit.type="submit";submit.name="submit";submit.value="submit";
+		//add an option element to a select element
+			function addOption(s,v,t){
+				var o=document.createElement("option");
+				o.value=v;o.innerHTML=t;
+				s.appendChild(o);
+			}
+		//populate the category select element
+			for(var key in categories)
+				addOption(category,key,key);
+		//initialize listener for category select element's onchange event
+			category.onchange=switchCategory();
+		//append select elements to the parent form
+			parent.appendChild(category);
+			parent.appendChild(number);
+			parent.appendChild(submit);
+		}(document.getElementById("search")));
+	</script>
+</body>
+</html>
